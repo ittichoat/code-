79bc746f7ce7f74bcc72b3cd186f5df01d486630
ittichoat <37753672+ittichoat@users.noreply.github.com>
2019-11-05 01:44:39 +0700

    edit

--- edabit/matchHouses.js
+++ edabit/matchHouses.js
@@ -2,7 +2,7 @@ function matchHouses(step) {
     var sum = 0
     for (var s = 1; s <= step; s++) {
         if (s == 1) {
-            sum++6
+            sum+=6
         } else {
             sum += 5
         }
