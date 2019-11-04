1059b1ac15e932ed9e0663d0222530ef4f8ae513
ittichoat <37753672+ittichoat@users.noreply.github.com>
2019-11-04 22:44:38 +0700

    matcgHouse"

---
+++ edabit/matchHouses.js
@@ -0,0 +1,11 @@
+function matchHouses(step) {
+    var sum = 0
+    for (var s = 1; s <= step; s++) {
+        if (s == 1) {
+            sum++6
+        } else {
+            sum += 5
+        }
+    }
+    return sum
+}
